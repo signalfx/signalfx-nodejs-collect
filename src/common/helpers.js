b1ac73da1229aa473531eccc0034b5a497419f03
Seon Kim <skim@signalfx.com>
2019-07-23 12:53:18 -0400

    Add node_version dimension

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -2,10 +2,20 @@
 
 const fqdn = require('fqdn');
 const { METRIC_SOURCE } = require('./constants');
+let node_version;
+
+if (process.versions) {
+  node_version = process.versions.node;
+}
+else {
+  node_version = 'unknown';
+}
+
 
 module.exports.getBasicDimensions = () => {
   return {
     host: fqdn(),
-    metric_source: METRIC_SOURCE
+    metric_source: METRIC_SOURCE,
+    node_version
   };
 };
