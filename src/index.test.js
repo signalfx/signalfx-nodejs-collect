3dac2cc7ab4764c9dfe1e7c288763a61c3f44dfc
Jakub Malinowski <jmalinowski+alt@splunk.com>
2021-09-03 16:51:22 +0200

    test: test datapoints shape

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const { json: bodyParserJson } = require('body-parser');
+
 const signalfx = require('signalfx');
 
 const SignalFxCollect = require('./index');
+
+function delay(ms) {
+  return new Promise(resolve => { setTimeout(resolve, ms); });
+}
 
 describe('basic usage scenarios', () => {
   let endpoint;
@@ -10,6 +16,8 @@ describe('basic usage scenarios', () => {
 
   beforeEach(() => {
     const app = express();
+    app.use(bodyParserJson());
+
     receivedDatapoints = [];
 
     // prevent accidental leakage between test cases
@@ -35,20 +43,70 @@ describe('basic usage scenarios', () => {
   });
 
   test('should launch without errors', async () => {
-    const client = new signalfx.Ingest('<token>', {
+    const client = new signalfx.IngestJson('<token>', {
       ingestEndpoint: endpoint
     });
 
     const collect = new SignalFxCollect({
       signalFxClient: client,
-      interval: 20
+      interval: 30
     });
     collect.start();
 
-    await new Promise(resolve => { setTimeout(resolve, 20 * 5); });
+    await delay(30 * 5);
     collect._stop();
 
     expect(receivedDatapoints.length).toBeGreaterThanOrEqual(3);
     expect(receivedDatapoints.length).toBeLessThanOrEqual(7);
+  });
+
+  test('should produce approximately correct data points', async () => {
+    const client = new signalfx.IngestJson('<token>', {
+      ingestEndpoint: endpoint
+    });
+
+    const collect = new SignalFxCollect({
+      signalFxClient: client,
+      interval: 30
+    });
+    collect.start();
+
+    await delay(50);
+    collect._stop();
+
+    const dataPoint = receivedDatapoints[0];
+    expect(dataPoint).toBeTruthy();
+
+    [
+      'nodejs.memory.gc.size',
+      'nodejs.memory.gc.pause',
+      'nodejs.memory.gc.total',
+      'nodejs.memory.gc.size',
+      'nodejs.memory.gc.pause',
+      'nodejs.memory.gc.total'
+    ].forEach(metricName => {
+      const counter = dataPoint.cumulative_counter.find(m => m.metric == metricName);
+      expect(counter).toBeTruthy();
+      expect(counter.value).toBeGreaterThanOrEqual(0);
+      expect(counter.timestamp).toBeGreaterThan(0);
+    });
+
+    [
+      'nodejs.cpu.utilization.system',
+      'nodejs.cpu.utilization.user',
+      'nodejs.cpu.utilization.total',
+      'nodejs.memory.system.total',
+      'nodejs.memory.system.free',
+      'nodejs.memory.heap.total',
+      'nodejs.memory.heap.used',
+      'nodejs.memory.rss',
+      'nodejs.event_loop.max',
+      'nodejs.event_loop.min'
+    ].forEach(metricName => {
+      const gauge = dataPoint.gauge.find(g => g.metric == metricName);
+      expect(gauge).toBeTruthy();
+      expect(gauge.value).toBeGreaterThanOrEqual(0);
+      expect(gauge.timestamp).toBeGreaterThan(0);
+    });
   });
 });
